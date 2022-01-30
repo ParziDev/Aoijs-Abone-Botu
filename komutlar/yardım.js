0231d475911eeb27a2c3f584208efc25e85cceea
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-30 16:17:16 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "yardım",
+  code: `
+  $author[$userTag;$authorAvatar]
+  $description[**$getServerVar[prefix]abone ver/al :** Üyeye abone rolü verip alır.
+**$getServerVar[prefix]abone-rol :** Abone rolünü ayarlarsınız.
+**$getServerVar[prefix]abone-yetkili :** Abone yetkilisini ayarlarsınız.
+**$getServerVar[prefix]abone-log :** Abone logunu ayarlarsınız.
+**$getServerVar[prefix]abone-kanal :** Abone kanalını ayarlarsınız.
+**$getServerVar[prefix]abone-sayı :** Abone sayısına bakarsınız.]
+  $color[BLACK]
+  $thumbnail[$authorAvatar]
+  `
+};
